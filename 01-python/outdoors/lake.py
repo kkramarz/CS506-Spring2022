a8aa1e9d363a19a89ff8fa928ec85f515f6a903f
katie kramarz <kkramarz@bu.edu>
2022-02-01 11:30:52 -0500

    Lake ASCII

--- 01-python/outdoors/lake.py
+++ 01-python/outdoors/lake.py
@@ -1,3 +1,6 @@
 def draw_lake():
-    print("lake not found")
+    lake = "     c@@88@c\n   c@@@888@@@c   cc\n c@@88@@88@@@c c888c\n c@@@8@@8888@@@@88@@c        \n  c@@88888@@@c c@@@c         \n    c@@\@8@@@c  ccc         \n        \   \   //          \n         \   \ //          \n          |    |    \n          |  D |\n          |    |\n          |    |\nwwwwwwwww/      \wwwwww________________wwwwww\n                      /    ~~*     **~~\ \n                   __/ ~*       ~~    ~ \ \n                  /         ~*    ~~     | \n               __/  ~~~*    ~~**   ~~     \ \n              /  ~~       ~~           ~~~ |\n             |       ~~~**     **~~~       |\n             |  ~~~       ~          ~~~   |\n              \____________________________/"
+    print(lake)
     return
+
+draw_lake()
